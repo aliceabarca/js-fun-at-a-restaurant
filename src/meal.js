b4cb0bf60725f37fffb87604834492c3269f4cca
Alice Abarca <aliceabarca@otmail.com>
2023-05-10 16:38:05 -0600

    Add the problem solving to meal test js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
 
+function addIngredients(topping, ingredients) {
+  if (!ingredients.includes(topping)) {
+  ingredients.push(topping)
+  }
+  return ingredients
+}
+
+function formatPrice(price) {
+  return `$${price}`
+}
+
+function decreasePrice(menu) {
+  menu *= .9
+  return menu
+}
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
