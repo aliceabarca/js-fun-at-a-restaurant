c73760cb9c8083b56154435fafd673b236a0628a
Alice Abarca <aliceabarca@otmail.com>
2023-05-10 18:38:55 -0600

    Add the first two functions in the order js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,25 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+  deliveryOrders.push(order)
+  }
+// return deliveryOrders
+}
 
+function refundOrder(orderNumber, deliveryOrders) {
+  console.log(deliveryOrders)
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1)
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  console.log(deliveryOrders)
+}
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
